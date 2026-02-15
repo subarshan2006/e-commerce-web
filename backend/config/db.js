b7bbcd4bab29e0326419829c27b4b774be176710
subarshan <subarshan195@gmail.com>
2026-02-15 12:53:40 +0530

    i change some think

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: happened ${error.message}`);
