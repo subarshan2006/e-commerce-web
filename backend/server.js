a6b0d64ca847670937daff24007cbd39f81a13ca
subarshan <subarshan195@gmail.com>
2026-02-15 12:59:25 +0530

    fix the error

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'http://e-commerce-web-6ai5.onrender.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
